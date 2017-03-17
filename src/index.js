338f362129331840bfe8a0b0863ef9f9d138fec4
Wasiff <wasiff@gmail.com>
2017-03-17 14:01:58 -0700

    refactor APP from functional to class component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API = 'AIzaSyDu1kyvI6WQ0WlKZJey_L8aM4mGpawavkg';
 
-YTSearch({ key: API, term: 'clapton' }, data => console.log(data));
 
-const APP = () => (
-  <div>
-      Learning React.jsx
-      <hr />
-    <SearchBar />
-  </div>
-  );
+class APP extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API, term: 'clapton' }, videos => this.setState({ videos }));
+  }
+  render() {
+    return (
+      <div>
+          Learning React.jsx
+          <hr />
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<APP />, document.querySelector('.container'));
 
